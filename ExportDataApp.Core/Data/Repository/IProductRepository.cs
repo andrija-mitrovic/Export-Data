334a4763de9945b28d0942b8570b6ec1cb3e9ef6
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-03-22 16:34:17 +0100

    Add repository

---
+++ ExportDataApp.Core/Data/Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace ExcelReportApp.Core.Data.Repository
+{
+    public interface IProductRepository
+    {
+        DataTable GetAllProducts(IDbConnection connection);
+        void CreateTable(string query, IDbConnection connection);
+        void InsertProducts(string tableName, DataTable dt, IDbConnection connection);
+    }
+}
---
+++ ExportDataApp.Core/Data/Repository/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using Advantage.Data.Provider;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReportApp.Core.Data.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public void CreateTable(string query, IDbConnection connection)
+        {
+            connection.ExecuteCreateQuery(query);
+        }
+
+        public DataTable GetAllProducts(IDbConnection connection)
+        {
+            string query = "SELECT sifra, barco, naziv, jedmj, pakov, zempo, roktr FROM R_ROBA";
+
+            return connection.ExecuteSelectQuery(query);
+        }
+
+        public void InsertProducts(string tableName, DataTable dt, IDbConnection connection)
+        {
+            string str = "";
+            for (int i = 0; i < 100; i++)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    str = $"INSERT INTO {tableName} (sifra,barco,naziv,jedmj,pakov,zempo,roktr) VALUES (";
+                    str += row["sifra"] + ",";
+                    str += "'" + row["barco"].ToString().Trim(' ') + "',";
+                    str += "'" + row["naziv"].ToString().Trim(' ') + "',";
+                    str += "'" + row["jedmj"].ToString().Trim(' ') + "',";
+                    str += row["pakov"].ToString().Replace(".", "").Replace(",", ".") + ",";
+                    str += "'" + row["zempo"].ToString().Trim(' ') + "',";
+                    str += row["roktr"] + ");";
+
+                    connection.ExecuteInsertQuery(str);
+                }
+            }
+        }
+    }
+}
