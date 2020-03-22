0207d009d63b2c3c30d12ee2c051c4dc4a5482f0
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-03-22 16:31:29 +0100

    Initial Creation

---
+++ ExportDataApp/Main.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ExportDataApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvView = new System.Windows.Forms.DataGridView();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.btnCsv = new System.Windows.Forms.Button();
+            this.btnDbf = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Products";
+            // 
+            // dgvView
+            // 
+            this.dgvView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvView.Location = new System.Drawing.Point(43, 90);
+            this.dgvView.Name = "dgvView";
+            this.dgvView.RowTemplate.Height = 24;
+            this.dgvView.Size = new System.Drawing.Size(1027, 546);
+            this.dgvView.TabIndex = 1;
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(43, 665);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(140, 35);
+            this.btnExcel.TabIndex = 3;
+            this.btnExcel.Text = "Export excel";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            // 
+            // btnCsv
+            // 
+            this.btnCsv.Location = new System.Drawing.Point(202, 665);
+            this.btnCsv.Name = "btnCsv";
+            this.btnCsv.Size = new System.Drawing.Size(140, 35);
+            this.btnCsv.TabIndex = 4;
+            this.btnCsv.Text = "Export csv";
+            this.btnCsv.UseVisualStyleBackColor = true;
+            // 
+            // btnDbf
+            // 
+            this.btnDbf.Location = new System.Drawing.Point(359, 665);
+            this.btnDbf.Name = "btnDbf";
+            this.btnDbf.Size = new System.Drawing.Size(140, 35);
+            this.btnDbf.TabIndex = 5;
+            this.btnDbf.Text = "Export dbf";
+            this.btnDbf.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1109, 729);
+            this.Controls.Add(this.btnDbf);
+            this.Controls.Add(this.btnCsv);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.dgvView);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvView;
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.Button btnCsv;
+        private System.Windows.Forms.Button btnDbf;
+    }
+}
+
